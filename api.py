6d13a9a9510832262ca4ad4610b8c9be4d5f832a
eduardacsouza <eduarda.souza@sou.inteli.edu.br>
2024-05-05 21:21:08 -0300

    feat: testes unitários

--- api.py
+++ api.py
@@ -4,12 +4,37 @@ class Animal:
         self.especie = especie
         self.nivel_felicidade = nivel_felicidade
 
+class Recinto:
+    def __init__(self, animais, bem_cuidado):
+        self.animais = animais
+        self.bem_cuidado = bem_cuidado
+
+    def adicionar_animal(self, animal):
+        self.animais.append(animal)
+
+    def remover_animal(self, animal):
+        self.animais.remove(animal)
+
+    def alimentar_animais(self):
+        for animal in self.animais:
+            animal.nivel_felicidade += 1
 
 class API:
     def __init__(self):
         self.animais = []
+        self.recintos = []
 
     def criar_animal(self, nome, especie, nivel_felicidade):
         animal = Animal(nome, especie, nivel_felicidade)
         self.animais.append(animal)
         return animal
+
+    def criar_recinto(self, animais, bem_cuidado):
+        recinto = Recinto(animais, bem_cuidado)
+        self.recintos.append(recinto)
+        return recinto
+
+    def receber_visitantes(self):
+        visitantes = sum([len(recinto.animais) for recinto in self.recintos])
+        dinheiro_ganho = visitantes * 4  
+        return dinheiro_ganho
